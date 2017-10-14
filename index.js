0764054b8926829988b371af48910524cb2a5f0c
Zak Bonnington <zakbonnington@Zaks-MacBook-Pro.local>
2017-10-14 13:26:51 +0100

    Set up for Goofspiel

--- index.js
+++ index.js
@@ -7,8 +7,28 @@ const app = express()
 app.use(express.static(__dirname + '/public'))
 
 // Handles all routes so you do not get a not found error
-app.get('*', function (request, response){
+app.get('/', function (request, response){
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
+app.get('/goofspiel', function (request, response){
+    response.sendFile(path.resolve(__dirname, 'public', 'goofspiel.html'))
+})
+
+var gameState = {
+    zak:{
+        cardsToPlay: [1,2,3,4,5,6,7,8,9,10,11,12,13],
+        cardsWon: []
+    },
+    peter:{
+        cardsToPlay:[1,2,3,4,5,6,7,8,9,10,11,12,13],
+        cardsWon:[]
+    },
+    cardsToPlayFor:[1,2,3,4,5,6,7,8,9,10,11,12,13]
+}
+
+app.get('/goofspiel/state', function (request, response){
+    response.send(JSON.stringify(gameState))
 })
 
 app.listen(port)
